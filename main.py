bff627dae06183b4f75c1e4410dd1299ccb61053
Katerina Kourti <katherinekourti@gmail.com>
2021-12-07 22:41:18 +0200

    removed comments & applied black

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from pyspark.sql import SparkSession
 from pyspark import SparkContext
 from pyspark.sql.types import *
 
-from pyspark.sql.functions import col,avg,round
-from os import path,getcwd
+from pyspark.sql.functions import col, avg, round
+from os import path, getcwd
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-# spark = SparkSession.builder.appName('Spark-IMDb-Project').config('spark.sql.analyzer.failAmbiguousSelfJoin',False).getOrCreate()
-spark = SparkSession.builder.appName('Spark-IMDb-Project').getOrCreate()
+spark = SparkSession.builder.appName("Spark-IMDb-Project").getOrCreate()
 
 movies_genre_file = "data/MOVIES_GENRE.txt"
 user_movies_file = "data/USER_MOVIES.txt"
@@ -81,91 +80,113 @@ final_joined = (
 # Creating the cube for the final joined table on dimensions 'genre' and 'gender',
 # aggregated data base on average of 'rating' column
 final_cubed = (
-    final_joined.cube(col('genre'), col('gender'))
-    .agg(round(avg(col("rating")),2).alias("avg_rating"))
+    final_joined.cube(col("genre"), col("gender"))
+    .agg(round(avg(col("rating")), 2).alias("avg_rating"))
     .orderBy("genre", "gender")
 )
 final_cubed.show()
 
 #### ----------------------------------------------------------- Query 1 -------------------------------------------------------------------
-folder = 'cube-results'
-filepath = path.join(folder,'{}')
+folder = "cube-results"
+filepath = path.join(folder, "{}")
 
 # Calculating each Group By produced by the cube
 # ----------------------------------------------------------------------------------------
 # Group By 'genre'
-gb_genre = final_cubed.filter(final_cubed.genre.isNotNull() & final_cubed.gender.isNull())
+gb_genre = final_cubed.filter(
+    final_cubed.genre.isNotNull() & final_cubed.gender.isNull()
+)
 pd_gb_genre = gb_genre.toPandas()
-pd_gb_genre.to_csv(filepath.format('Genre.csv'),index=False)
+pd_gb_genre.to_csv(filepath.format("Genre.csv"), index=False)
 gb_genre.show(truncate=False)
 
 # ----------------------------------------------------------------------------------------
 # Group By 'gender'
-gb_gender = final_cubed.filter(final_cubed.gender.isNotNull() & final_cubed.genre.isNull())
+gb_gender = final_cubed.filter(
+    final_cubed.gender.isNotNull() & final_cubed.genre.isNull()
+)
 pd_gb_gender = gb_gender.toPandas()
-pd_gb_gender.to_csv(filepath.format('Gender.csv'),index=False)
+pd_gb_gender.to_csv(filepath.format("Gender.csv"), index=False)
 gb_gender.show(truncate=False)
 
 # ----------------------------------------------------------------------------------------
 # Group By ('genre','gender')
-gb_genre_gender = final_cubed.filter(final_cubed.gender.isNotNull() & final_cubed.genre.isNotNull())
+gb_genre_gender = final_cubed.filter(
+    final_cubed.gender.isNotNull() & final_cubed.genre.isNotNull()
+)
 pd_gb_genre_gender = gb_genre_gender.toPandas()
-pd_gb_genre_gender.to_csv(filepath.format('Genre_Gender.csv'),index=False)
+pd_gb_genre_gender.to_csv(filepath.format("Genre_Gender.csv"), index=False)
 gb_genre_gender.show(truncate=False)
 
 # ----------------------------------------------------------------------------------------
 # Group By 'none'
 gb_none = final_cubed.filter(final_cubed.gender.isNull() & final_cubed.genre.isNull())
 pd_gb_none = gb_none.toPandas()
-pd_gb_none.to_csv(filepath.format('None.csv'),index=False)
+pd_gb_none.to_csv(filepath.format("None.csv"), index=False)
 gb_none.show(truncate=False)
 
 
 #### ----------------------------------------------------------- Query 2 ---------------------------------------------------------------
 
-# We will use the Group By ('genre','gender') 
-female_ratings = gb_genre_gender.withColumnRenamed('avg_rating', 'female_avg_rating').filter(gb_genre_gender.gender == 'F')
-male_ratings = gb_genre_gender.withColumnRenamed('avg_rating', 'male_avg_rating').filter(gb_genre_gender.gender == 'M')
-joined = male_ratings.join(female_ratings, ['genre'])
-joined.createOrReplaceTempView('GenreRatingsPerGender')
+# We will use the Group By ('genre','gender')
+female_ratings = gb_genre_gender.withColumnRenamed(
+    "avg_rating", "female_avg_rating"
+).filter(gb_genre_gender.gender == "F")
+male_ratings = gb_genre_gender.withColumnRenamed(
+    "avg_rating", "male_avg_rating"
+).filter(gb_genre_gender.gender == "M")
+joined = male_ratings.join(female_ratings, ["genre"])
+joined.createOrReplaceTempView("GenreRatingsPerGender")
 
-data = spark.sql('SELECT genre FROM GenreRatingsPerGender WHERE female_avg_rating > male_avg_rating')
+data = spark.sql(
+    "SELECT genre FROM GenreRatingsPerGender WHERE female_avg_rating > male_avg_rating"
+)
 data.show()
 
 
 # ### ---------------------------------------------------------- Query 3 -------------------------------------------------------------
-# select count() group by rating, the chosen genre is 'Adventure 
-rating_per_genre = final_joined.where(col('genre') == 'Adventure').groupBy(col('rating')).count().withColumnRenamed("count","total_ratings").orderBy(col('rating'))
+# select count() group by rating, the chosen genre is 'Adventure
+rating_per_genre = (
+    final_joined.where(col("genre") == "Adventure")
+    .groupBy(col("rating"))
+    .count()
+    .withColumnRenamed("count", "total_ratings")
+    .orderBy(col("rating"))
+)
 rating_per_genre.show()
 
 ratings_df = rating_per_genre.toPandas()
 
-labels =  ratings_df.rating
+labels = ratings_df.rating
 sizes = ratings_df.total_ratings
 
-# colors = ['#ff9999','#66b3ff','#99ff99','#ffcc99','#F9E79F']
-colors = ['#F3A935','#2586a4','#6ebe9f','#55596a','#c73558']
+colors = ["#F3A935", "#2586a4", "#6ebe9f", "#55596a", "#c73558"]
 fig, ax = plt.subplots()
 
+
 def func(pct, allvals):
-    absolute = int(np.round(pct/100.*np.sum(allvals)))
+    absolute = int(np.round(pct / 100.0 * np.sum(allvals)))
     return "{:.1f}%\n({:d})".format(pct, absolute)
 
 
-wedges, texts, autotexts = ax.pie(sizes,labels=labels, colors=colors, autopct=lambda pct: func(pct, sizes),
-                                  textprops=dict(color="w"))
+wedges, texts, autotexts = ax.pie(
+    sizes,
+    labels=labels,
+    colors=colors,
+    autopct=lambda pct: func(pct, sizes),
+    textprops=dict(color="w"),
+)
 
-ax.legend(wedges, labels,
-          title="Ratings",
-          loc="center left",
-          bbox_to_anchor=(1, 0, 0.5, 1))
+ax.legend(
+    wedges, labels, title="Ratings", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1)
+)
 
 plt.setp(autotexts, size=8, weight="bold")
 
 ax.set_title("Total count number per rating for genre 'Adventure'")
 
 # plt.show()
-plt.savefig('ratings_pie.png')
+plt.savefig("ratings_pie.png")
 
 # Clear plt
 plt.clf()
@@ -173,13 +194,12 @@ plt.clf()
 ##################################################
 # Chart bars
 
-fig1 = plt.figure(figsize = (10, 5))
- 
+fig1 = plt.figure(figsize=(10, 5))
+
 # creating the bar plot
-plt.bar(labels, sizes, color = colors,
-        width = 0.4)
- 
+plt.bar(labels, sizes, color=colors, width=0.4)
+
 plt.xlabel("Rating")
 plt.ylabel("Total Number of Ratings")
 plt.title("Total count number per rating for genre 'Adventure'")
-plt.savefig('rating_bars.png')
+plt.savefig("rating_bars.png")
